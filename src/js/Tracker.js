74a550e721e4c1fbd865ce6edf2cf9a39173dfdf
algaba97 <jalgaba@ucm.es>
2019-06-20 20:07:04 +0200

    finished comments

--- src/js/Tracker.js
+++ src/js/Tracker.js
@@ -9,42 +9,44 @@ const uniqid = require('uniqid');
 class Tracker {
 
     constructor(typeOfPersistance, typeOfSerializing){
-      this.userid = uniqid();
-      this.event_queue = [];
+      this.userid = uniqid(); //Generacion de una id única para el usuario
+      this.event_queue = []; //Pila de eventos
 
       switch (typeOfPersistance) {
         case 0:
           this.Persistence = new ServerPersistance('http://localhost:8080/tracker');
           break;
-        case 1:
+        case 1://No está implementado
           this.Persistence = new DiskPersistance("log.txt");
           break;
-        default:
+        default://No está implementado
           this.Persistence = new DiskPersistance("log.txt");
           break;
       }
 
       switch (typeOfSerializing) {
-        case 0:
+        case 0: //Serialización en CSV
           this.Serializer = new CSVSerializer();
           break;
-        case 1:
+        case 1: //Serialización en JSON
           this.Serializer = new JSONSerializer();
           break;
         default:
           this.Serializer = new CSVSerializer();
           break;
       }
+        //Añadir un evento , necesitamos el EventType y la información adicional
       this.addEvent = function(event_type, event_info)
       {
         let date = new Date();
         let timestamp = date.getTime();
         let event = new Event(this.userid, timestamp, event_type, event_info)
         this.event_queue.push(event);
-        if(this.event_queue.length > 5)
+        if(this.event_queue.length > 5) //Si la pila es mayor de 5 guardamos
           this.saveWithPersistance();
   
       }
+       //funcion asíncrona que sirve para guardar los eventos de la pila
       this.saveWithPersistance = async function()
       {
         this.event_queue.forEach(event => {
@@ -61,7 +63,7 @@ class Tracker {
   }
 
   var Instance;
-
+  //inicializamos el tracker como un singletone
   function InitializeTracker(typeOfPersistance,typeOfSerializing){
     if(Instance == undefined){
       Instance = new Tracker(typeOfPersistance, typeOfSerializing);
@@ -71,10 +73,11 @@ class Tracker {
     }
   }
 
+  //añadir evento
   function AddEvent(event_type,event_info){
     Instance.addEvent(event_type,event_info);
   }
-
+  //guardar
   function SaveWithPersistance(){
     Instance.saveWithPersistance();
   }
